05b871544639665a36630a2cbcf8e1620337c161
Shrey Joshi <shreyjoshi2004@gmail.com>
2022-12-20 13:38:45 -0600

    better ui with chakra UI

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <RecoilRoot>
+  <RecoilRoot>
+    <ChakraProvider>
       <App />
-    </RecoilRoot>
+    </ChakraProvider>
+  </RecoilRoot>
   // </React.StrictMode>
 );
 
